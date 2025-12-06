ba35f773a1f9df18bdd42ec2f5a6f45c067d748e
Fakhar Sultan <fakharsultan.dev@gmail.com>
2025-12-06 22:29:47 +0500

    feat: add favicons, apple touch icon, and web app manifest, updating SeoHead to link them.

--- src/lib/components/SeoHead.svelte
+++ src/lib/components/SeoHead.svelte
@@ -13,6 +13,14 @@
 
 <svelte:head>
   <title>{title}</title>
+
+  <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+  <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+  <link rel="shortcut icon" href="/favicon.ico" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+  <meta name="apple-mobile-web-app-title" content="Fakhy" />
+  <link rel="manifest" href="/site.webmanifest" />
+
   <meta name="description" content={description} />
   <meta property="og:title" content={title} />
   <meta property="og:type" content="website">
